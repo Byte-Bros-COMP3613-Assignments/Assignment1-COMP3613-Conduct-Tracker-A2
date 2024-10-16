0f4c79199610491aff6023443a034eda3ac3f9e6
TealTrim <teal.trim20@gmail.com>
2024-10-16 15:31:19 +0000

    Added unit tests.

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -3,7 +3,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from App.main import create_app
 from App.database import db, create_db
-from App.models import User
+from App.models import User, Student, Review
 from App.controllers import (
     create_user,
     get_all_users_json,
@@ -22,25 +22,48 @@ LOGGER = logging.getLogger(__name__)
 class UserUnitTests(unittest.TestCase):
 
     def test_new_user(self):
-        user = User("bob", "bobpass")
-        assert user.username == "bob"
+        newUser = User("bob", "bobpass")
+        assert newUser.username == "bob"
 
     # pure function no side effects or integrations called
-    def test_get_json(self):
-        user = User("bob", "bobpass")
-        user_json = user.get_json()
-        self.assertDictEqual(user_json, {"id":None, "username":"bob"})
+    def test_user_get_json(self):
+        newUser = User("bob", "bobpass")
+        user_json = newUser.get_json()
+        self.assertDictEqual(user_json, {"id": None, "username": "bob"})
     
     def test_hashed_password(self):
         password = "mypass"
         hashed = generate_password_hash(password, method='sha256')
-        user = User("bob", password)
-        assert user.password != password
+        newUser = User("bob", password)
+        assert newUser.password != password
 
     def test_check_password(self):
         password = "mypass"
-        user = User("bob", password)
-        assert user.check_password(password)
+        newUser = User("bob", password)
+        assert newUser.check_password(password)
+
+    def test_new_student(self):
+        newStudent = Student("John Doe")
+        assert newStudent.id == None
+        assert newStudent.name == "John Doe"
+
+    def test_student_get_json(self):
+        newStudent = Student("John Doe")
+        student_json = newStudent.get_json()
+        self.assertDictEqual(student_json, {"id": None, "name": "John Doe", "reviews": []})
+
+    def test_new_review(self):
+        newReview = Review(1, "Positive comment about student.", True)
+        assert newReview.id == None
+        assert newReview.student_id == 1
+        assert newReview.comment == "Positive comment about student."
+        assert newReview.is_positive == True
+
+    def test_review_get_json(self):
+        newReview = Review(1, "Positive comment about student.", True)
+        review_json = newReview.get_json()
+        self.assertDictEqual(review_json, {"id": None, "student_id": 1, "comment": "Positive comment about student.", "is_positive": True})
+
 
 '''
     Integration Tests
@@ -75,5 +98,3 @@ class UsersIntegrationTests(unittest.TestCase):
         update_user(1, "ronnie")
         user = get_user(1)
         assert user.username == "ronnie"
-        
-
